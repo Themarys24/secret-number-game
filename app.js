751c6ecf61ee8a80d87b71e882128d6729434a51
Maria Ferreira <144544205+Themarys24@users.noreply.github.com>
2025-01-28 22:26:37 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+
+let listOfDrawnNumbers = [];
+
+let limitNumber = 10;
+let secretNumber = generateRandomNumber();
+let attempts = 1;
+
+function displayTextOnScreen(tag, text){
+    let camp = document.querySelector(tag);
+    camp.innerHTML = text;
+    responsiveVoice.speak(text, 'Brazilian Portuguese Female', {rate:1.2} );
+}
+
+function displayInitialMessage(){
+    displayTextOnScreen('h1', 'Secret Number Game');
+    displayTextOnScreen('p', 'Choose a number from 1 to 10');
+}
+
+displayInitialMessage();
+
+function checkTheKick(){
+    let kick = document.querySelector('input').value;
+    
+    if (kick == secretNumber){
+        displayTextOnScreen('h1', 'Got it right!');
+        let attemptWord = attempts > 1 ? 'attempts' : 'attempt';
+        let attemptMessage = `You figured out the secret number with ${attempts} ${attemptWord}`;
+        displayTextOnScreen('p', attemptMessage);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if (kick > secretNumber){
+            displayTextOnScreen('p', 'The secret number is smaller');
+        }else{
+            displayTextOnScreen('p','The secret number is bigger');
+        }
+        attempts ++;
+        clearField();
+
+    }
+}
+
+function generateRandomNumber(){
+    let chosenNumber = parseInt(Math.random() * limitNumber + 1);
+    let quantityOfElements = listOfDrawnNumbers.length;
+
+    if (quantityOfElements == limitNumber){
+        listOfDrawnNumbers = [];
+    }
+
+        if (listOfDrawnNumbers.includes(chosenNumber)){
+        return generateRandomNumber();
+    }else{
+        listOfDrawnNumbers.push(chosenNumber);
+        console.log(listOfDrawnNumbers);
+        return chosenNumber;
+    }
+}
+
+function clearField(){
+    kick = document.querySelector('input');
+    kick.value = ' '; 
+}
+
+function restartGame(){
+    secretNumber = generateRandomNumber();
+    clearField();
+    attempts = 1;
+    displayInitialMessage();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
